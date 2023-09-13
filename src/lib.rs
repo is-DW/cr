0d97a084f7c8640bfd16580643c34e32a3dd606c
is-DW <ldw66666520@gmail.com>
2023-09-13 17:55:10 +0800

    feat: add rgb, rgba struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[derive(Copy, Clone, Debug, Default, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub struct RGB<T> {
+    /// Red
+    pub r: T,
+    /// Green
+    pub g: T,
+    /// Blue
+    pub b: T,
 }
+
+#[derive(Copy, Clone, Debug, Default, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub struct RGBA<T, TA = T> {
+    /// Red
+    pub r: T,
+    /// Green
+    pub g: T,
+    /// Blue
+    pub b: T,
+    /// Alpha
+    pub a: TA,
+}
+
+pub type RGB8 = RGB<u8>;
+pub type RGB16 = RGB<u16>;
+
+pub type RGBA8 = RGBA<u8>;
+pub type RGBA16 = RGBA<u16>;
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+mod tests {}
